caaeb72ad62f3161f79052d4cd615bde0a688915
Usaid Ahmed <usaid33376@gmail.com>
2026-02-23 13:30:38 +0500

    Update load_to_mysql.py

--- scripts/load_to_mysql.py
+++ scripts/load_to_mysql.py
@@ -2,14 +2,13 @@ import pandas as pd
 import logging
 from sqlalchemy import create_engine
 
-
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
 USERNAME = "root"
-PASSWORD = "usaid123"  
+PASSWORD = "usaid123"
 HOST = "localhost"
 DATABASE = "ecommerce_db"
 
@@ -21,13 +20,25 @@ if __name__ == "__main__":
             f"mysql+pymysql://{USERNAME}:{PASSWORD}@{HOST}/{DATABASE}"
         )
 
+        logging.info("Connection established.")
+
+        
+        logging.info("Reading raw CSV file...")
+        raw_df = pd.read_csv("data/raw_ecommerce.csv")
+
+        logging.info("Loading raw data into MySQL...")
+        raw_df.to_sql("raw_orders", engine, if_exists="replace", index=False)
+
+        logging.info("Raw data loaded successfully.")
+
+    
         logging.info("Reading cleaned CSV file...")
-        df = pd.read_csv("data/cleaned_ecommerce.csv")
+        clean_df = pd.read_csv("data/cleaned_ecommerce.csv")
 
-        logging.info("Loading data into MySQL...")
-        df.to_sql("orders", engine, if_exists="replace", index=False)
+        logging.info("Loading cleaned data into MySQL...")
+        clean_df.to_sql("cleaned_orders", engine, if_exists="replace", index=False)
 
-        logging.info("Data successfully loaded into MySQL.")
+        logging.info("Cleaned data loaded successfully.")
 
     except Exception as e:
         logging.error(f"MySQL load failed: {e}")
